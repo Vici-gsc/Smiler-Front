6c8f26ea90f7e8eb43874469e9babf344bbeaef2
Mango-Juice <wjsalsrb5@naver.com>
2023-02-13 17:44:29 +0900

    :lipstick: feat : 메인 페이지 뷰 제작

---
+++ lib/presentation/component/organism/main_menu_list.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import '../atom/main_button.dart';
+
+class MainMenuList extends StatelessWidget {
+  final List<MainMenuListItem> items;
+
+  const MainMenuList({Key? key, required this.items}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: items
+            .map((item) => MainButton(text: item.text, onTap: item.onTap))
+            .toList(),
+      ),
+    );
+  }
+}
+
+class MainMenuListItem {
+  final String text;
+  final Function() onTap;
+
+  const MainMenuListItem({required this.text, required this.onTap});
+}
---
+++ lib/ui/service_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class ServiceColors {
+  static const Color primary = Color(0xFFFFC700);
+  static const Color primaryLight = Color(0xFFFFECBD);
+  static const Color secondary = Color(0xFFD9D9D9);
+  static const Color textColor = Color(0xFF333333);
+  static const Color secondaryText = Color(0xFF797979);
+}
