c70caa8b0fa1104a24f43e48051f7c9b22858830
Mango-Juice <wjsalsrb5@naver.com>
2023-02-21 22:09:11 +0900

    :poop: feat: 공용 에러 메시지 추가

--- lib/data/source/api.dart
+++ lib/data/source/api.dart
@@ -5,8 +5,12 @@ import 'package:http/http.dart' as http;
 import 'model/result.dart';
 
 class Api {
-  static const timeLimit = Duration(seconds: 10);
-  static const baseUrl = "localhost";
+  static const Duration timeLimit = Duration(seconds: 10);
+  static const String baseUrl = "localhost";
+  static const String defaultErrorMessage =
+      "오류가 발생하였습니다.\n이 오류가 계속 발생하는 경우 개발자에게 문의해주세요.";
+  static const String networkErrorMessage =
+      "네트워크 연결이 원활하지 않습니다.\n잠시 후 다시 시도해주세요.";
 
   final http.Client client;
 
@@ -43,8 +47,7 @@ class Api {
     if (response.statusCode ~/ 100 == 2) {
       return Result.success(jsonDecode(response.body));
     } else {
-      return Result.failure(
-          "${response.statusCode} Error: ${response.reasonPhrase}");
+      return Result.failure(defaultErrorMessage);
     }
   }
 
@@ -79,15 +82,15 @@ class Api {
               .timeout(timeLimit);
       }
 
+      print(response);
       if (response.statusCode ~/ 100 == 2) {
         return Result.success(jsonDecode(response.body));
       } else {
-        return Result.failure(
-            "${response.statusCode} Error: ${response.reasonPhrase}");
+        return Result.failure(defaultErrorMessage);
       }
     } catch (e) {
       print(e);
-      return const Result.failure("Internal Error");
+      return const Result.failure(networkErrorMessage);
     }
   }
 }
