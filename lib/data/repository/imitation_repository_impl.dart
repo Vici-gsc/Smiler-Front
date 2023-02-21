22787ae8f7901be4741e60becab1cba4910d15ab
Mango-Juice <wjsalsrb5@naver.com>
2023-02-21 23:16:45 +0900

    :memo: doc : src/data 폴더 코드들에 주석 추가

--- lib/data/repository/imitation_repository_impl.dart
+++ lib/data/repository/imitation_repository_impl.dart
@@ -2,16 +2,23 @@ import 'package:smiler/data/source/model/result.dart';
 import 'package:smiler/domain/model/scoring_result.dart';
 import 'package:smiler/domain/repository/imitation_repository.dart';
 
+import '../../domain/model/emotion.dart';
 import '../source/api.dart';
 
+/// [ImitationRepository]의 구현체입니다.
 class ImitationRepositoryImpl implements ImitationRepository {
-  final Api api;
+  final Api _api;
 
-  ImitationRepositoryImpl(this.api);
+  /// [ImitationRepositoryImpl] 클래스를 생성합니다.
+  ImitationRepositoryImpl(this._api);
 
+  /// [emotionName]에 해당하는 감정 이미지를 가져옵니다.
+  ///
+  /// [emotionName]은 미리 백엔드, 딥러닝 파트와 협의된 영단어를 사용합니다.
+  /// 자세한 내용은 [Emotion] 클래스를 참고하세요.
   @override
   Future<Result<String>> getEmotionImage(String emotionName) async {
-    final result = await api.get(
+    final result = await _api.get(
       "/imitation/photo?feeling=$emotionName",
     );
 
@@ -24,10 +31,14 @@ class ImitationRepositoryImpl implements ImitationRepository {
     );
   }
 
+  /// [imagePath]에 해당하는 이미지가 [answerEmotionName]에 해당하는 감정과 일치하는지 확인합니다.
+  ///
+  /// [answerEmotionName]은 미리 백엔드, 딥러닝 파트와 협의된 영단어를 사용합니다.
+  /// 자세한 내용은 [Emotion] 클래스를 참고하세요.
   @override
   Future<Result<ScoringResult>> isCorrectImitation(
       String answerEmotionName, String imagePath) async {
-    final result = await api.postFile(
+    final result = await _api.postFile(
       "/imitation/face?feeling=$answerEmotionName",
       imagePath,
     );
